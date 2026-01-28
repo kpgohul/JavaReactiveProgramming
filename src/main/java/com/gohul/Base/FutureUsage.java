4312f5d669f28e90fd4f4ae109bc4ccbf5ae322d
Gohul K <144474239+kpgohul@users.noreply.github.com>
2026-01-28 16:04:00 +0530

    updated info for future

--- src/main/java/com/gohul/Base/FutureUsage.java
+++ src/main/java/com/gohul/Base/FutureUsage.java
@@ -9,10 +9,12 @@ import java.util.concurrent.*;
 
 public class FutureUsage {
 
-    // Future Disadvantage,
-    // cann't run async ny itself, 
+    // Future Disadvantages:
+    // cann't run async by itself, 
     // doesn't support callback and chaining the result
     // .get() will block the thread!
+
+    // Note: We have to use ExecutorService here to achive the non-blocking nature
 
     // Runnable - execution + doesn't has return value
     // Callable - execution + has return value
